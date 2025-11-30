b146fc09dd7db95a8ea9cbbaf209efe4c4b4d6b1
WichuDoesCode <simeon062007@gmail.com>
2025-11-30 21:44:36 +0200

    fixed bugs

---
+++ build/CMakeFiles/SynapseVisionLab.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SynapseVisionLab.dir/main.cpp.obj"
+  "CMakeFiles/SynapseVisionLab.dir/main.cpp.obj.d"
+  "SynapseVisionLab.exe"
+  "SynapseVisionLab.exe.manifest"
+  "SynapseVisionLab.pdb"
+  "libSynapseVisionLab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SynapseVisionLab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SynapseVisionLab.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SynapseVisionLab.
+# This may be replaced when dependencies are built.
